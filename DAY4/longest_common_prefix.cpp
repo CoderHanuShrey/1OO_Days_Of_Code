b8102fe70b4346b7056c2ca89cf53e6b995d1933
CoderHanuShrey <shreyas27patondikar@gmail.com>
2023-11-24 22:41:55 +0530

    initial commit

---
+++ DAY4/longest_common_prefix.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string prefix = strs[0];
+        for (string s : strs)
+            while (s.find(prefix) != 0)
+                prefix = prefix.substr(0, prefix.length() - 1);
+        return prefix;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<string> strs = {"flower","flow","flight"};
+    cout << s.longestCommonPrefix(strs) << endl;
+    return 0;
+}
